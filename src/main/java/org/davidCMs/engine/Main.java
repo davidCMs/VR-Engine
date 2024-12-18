cd46bc8bcb0767c0329b56dcb44b10f3809e4b7a
davidCMs <gregor.cink@gmail.com>
2024-12-18 18:35:10 +0100

    many'o'things

--- src/main/java/org/davidCMs/engine/Main.java
+++ src/main/java/org/davidCMs/engine/Main.java
@@ -7,19 +7,13 @@ import org.davidCMs.engine.scene.gameobject.GameObject;
 import org.davidCMs.engine.scene.gameobject.JiggleComponent;
 import org.davidCMs.engine.scene.gameobject.components.CameraComponent;
 import org.davidCMs.engine.scene.gameobject.components.MeshComponent;
-import org.davidCMs.engine.utils.EasingFunctions;
 import org.davidCMs.engine.utils.GlobalRandom;
+import org.davidCMs.engine.utils.LogList;
 import org.davidCMs.engine.utils.ModelLoader;
-import org.davidCMs.engine.window.GLFWException;
 import org.davidCMs.engine.scene.gameobject.components.transform.TransformComponent;
 import org.davidCMs.engine.openxr.*;
 import org.davidCMs.engine.render.model.Mesh;
-import org.davidCMs.engine.render.renderer.oglobjects.EBO;
 import org.davidCMs.engine.render.renderer.oglobjects.GLDrawType;
-import org.davidCMs.engine.render.renderer.oglobjects.VAO;
-import org.davidCMs.engine.render.renderer.oglobjects.VBO;
-import org.joml.Matrix4f;
-import org.joml.Vector2f;
 import org.joml.Vector3f;
 import org.joml.Vector4f;
 import org.lwjgl.PointerBuffer;
@@ -30,6 +24,9 @@ import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.opengl.*;
 import org.lwjgl.openxr.*;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import static org.lwjgl.glfw.GLFW.*;
@@ -45,6 +42,15 @@ public class Main {
 
     public static XrEventDataBuffer eventDataBuffer;
 
+    private static AtomicBoolean update = new AtomicBoolean(false);
+
+    private static final int TARGET_UPS = 60;
+    private static Scene scene;
+
+    private static final LogList FPSLog = new LogList(100);
+    private static final LogList UPSLog = new LogList(10);
+
+
     public static void main(String[] args) {
 
         if (!glfwInit())
@@ -55,7 +61,7 @@ public class Main {
 
         GameWindow gameWindow = new GameWindow();
 
-        Scene scene = new Scene();
+        scene = new Scene();
         SceneRenderer sceneRenderer = new SceneRenderer(scene);
 
         //openXRInstance = new OpenXRInstance();
@@ -82,13 +88,18 @@ public class Main {
         gameCamera.addComponent(gameCameraCameraComponent);
         gameCameraCameraComponent.update();
         
-        int size = 30;
 
-        AIScene aiScene = Assimp.aiImportFile("cube.obj", Assimp.aiProcess_Triangulate);
+
+        AIScene aiScene = Assimp.aiImportFile("model.obj", Assimp.aiProcess_Triangulate);
+        if (aiScene == null) {
+            System.err.println("No cube.obj in current dir");
+            System.exit(-1);
+        }
         PointerBuffer buffer = aiScene.mMeshes();
 
         Mesh mesh = ModelLoader.loadMesh(AIMesh.create(buffer.get(0)), GLDrawType.STATIC);
 
+        int size = 2;
         int iter = 0;
 
         for (int i = size/2*-1; i < size/2; i++) {
@@ -126,14 +137,13 @@ public class Main {
 
         System.out.println("looped " + iter + " times");
 
-        AtomicBoolean update = new AtomicBoolean(false);
+
 
         gameWindow.addKeyCallback((window, key, scancode, action, mods) -> {
             if (key == GLFW_KEY_SPACE && action == GLFW_PRESS)
                 update.set(!update.get());
         });
 
-        float i = 10;
         glfwSwapInterval(0);
 
         double lastTime = System.nanoTime() / 1_000_000_000.0;
@@ -143,23 +153,20 @@ public class Main {
         GL11.glEnable(GL11.GL_BLEND);
         GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 
-        int displayUpdate = 0;
+        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
+        Runnable updateRunnable = getUpdaterRunnable();
+        executorService.scheduleAtFixedRate(updateRunnable,0, 1000/TARGET_UPS, TimeUnit.MILLISECONDS);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(executorService::shutdown));
+
+        System.gc();
 
         while (!gameWindow.shouldClose()) {
             double currentTime = System.nanoTime() / 1_000_000_000.0;
             deltaTime = currentTime - lastTime;
             lastTime = currentTime;
 
-            if (!gameWindow.hasContext())
-                throw new GLFWException("no context?!?!?!!?!?!?!?!?");
-
             //gameObject.getComponent(TransformComponent.class).setRotation(new Quaternionf().rotateXYZ((float) 0, (float) 0, (float) ((float) i* 0.001)));
-            if (update.get()) {
-                scene.update(deltaTime);
-                TransformComponent gct = gameCamera.getComponent(TransformComponent.class);
-                gct.setPosition(gct.getPosition().add((float) 0, (float) 0, (float) (-0.1f*deltaTime)));
-
-            }
 
             gameCameraCameraComponent.update();
             GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
@@ -170,17 +177,43 @@ public class Main {
 
             gameWindow.swapBuffers();
 
-            i += (float) deltaTime;
+            double fps = 1 / deltaTime;
+            FPSLog.add(fps);
 
-            if (displayUpdate >= 10) {
-                gameWindow.setTitle("Game, FPS = " + 1 / deltaTime);
-                displayUpdate = 0;
-            }
-            displayUpdate++;
+            double avgFps = FPSLog.getAverage();
+            double avgUps = UPSLog.getAverage();
+            gameWindow.setTitle("Game, FPS = " + avgFps + ",\t UPS = " + avgUps);
 
             glfwPollEvents();
         }
+        executorService.shutdown();
+        glfwPollEvents();
     }
 
+    private static final long[] lastExecutionTime = {System.nanoTime()};
+    private static final long[] lastSecondTime = {System.nanoTime()};
+    private static int ups = 0;
+    public static Runnable getUpdaterRunnable() {
+        return () -> {
+
+            long currentTime = System.nanoTime();
+            double timeDelta = (currentTime - lastExecutionTime[0]) / 1_000_000_000.0;
+
+            ups++;
+
+            if ((currentTime - lastSecondTime[0]) >= 1_000_000_000L) {
+                UPSLog.add(ups);
+                ups = 0;
+                lastSecondTime[0] = currentTime;
+            }
+
+            lastExecutionTime[0] = currentTime;
+
+            if (update.get()) {
+                scene.update(timeDelta);
+            }
+
+        };
+    }
 
 }
